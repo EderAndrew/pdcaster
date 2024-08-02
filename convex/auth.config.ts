230036bee0469566d2ecbc7ef69f4272ca6633a6
Eder Andrew <ederandrew0028@gmail.com>
2024-08-02 11:13:47 -0300

    clerk configuration

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+//https://moral-bull-45.clerk.accounts.dev
+const authConfig = {
+    providers: [
+      {
+        domain: "https://moral-bull-45.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+};
+
+export default authConfig
